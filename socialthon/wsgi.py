17a76cbd617b8ea1c5503c54f48f67700382cabd
DevMedo <idevmedo@gmail.com>
2021-02-19 19:25:31 +0300

    changes to env

--- socialthon/wsgi.py
+++ socialthon/wsgi.py
@@ -6,11 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
-
 import os
+import sys
 
+path = '/home/socialthon/socialthon'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'socialthon.settings'
+
+# then:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialthon.settings')
-
 application = get_wsgi_application()
